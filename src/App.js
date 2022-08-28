61d60b2f136a5c1f435874889b23ad506208ce98
Muralidhar4143 <ragitimuralidhar@gmail.com>
2022-08-28 13:50:00 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,93 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+const App = () => {
+	const [result, setResult] = useState("");
+	const changeHandler = (e) => {
+		setResult(result.concat(e.target.name));
+	};
+	const clear = () => {
+		setResult("");
+	};
+	const backspace = () => {
+		setResult(result.slice(0, result.length - 1));
+	};
+	const calculate = () => {
+		try {
+			setResult(eval(result).toString());
+		} catch (err) {
+			setResult("Error");
+		}
+	};
+
+	return (
+		<div className="container">
+			<center>
+				<h1>CALCULATOR</h1>
+				<form>
+					<input type="text" value={result} />
+					<button className="imp_buttons" onClick={clear} id="clear">CLEAR</button>
+				</form>
+        <div className="Buttons">
+        <button name="1" onClick={changeHandler}>
+					1
+				</button>
+				<button name="2" onClick={changeHandler}>
+					2
+				</button>
+				<button name="3" onClick={changeHandler}>
+					3
+				</button>
+				<button className="imp_buttons" name="/" onClick={changeHandler}>
+					&divide;
+				</button>
+
+				<button name="4" onClick={changeHandler}>
+					4
+				</button>
+				<button name="5" onClick={changeHandler}>
+					5
+				</button>
+				<button name="6" onClick={changeHandler}>
+					6
+				</button>
+				<button className="imp_buttons" name="-" onClick={changeHandler}>
+					&ndash;
+				</button>
+
+
+				<button name="7" onClick={changeHandler}>
+					7
+				</button>
+				<button name="8" onClick={changeHandler}>
+					8
+				</button>
+				<button name="9" onClick={changeHandler}>
+					9
+				</button>
+				<button className="imp_buttons" name="+" onClick={changeHandler}>
+					+
+				</button>
+
+
+				<button name="." onClick={changeHandler}>
+					.
+				</button>
+				<button name="0" onClick={changeHandler}>
+					0
+				</button>
+				<button onClick={calculate} id="answer">=</button>
+				<button className="imp_buttons" name="*" onClick={changeHandler}>
+					&times;
+				</button>
+
+				<button className="imp_buttons" onClick={backspace} id="backspace">BACKSPACE</button>
+        </div>
+				
+			</center>
+		</div>
+	);
+};
 
 export default App;
